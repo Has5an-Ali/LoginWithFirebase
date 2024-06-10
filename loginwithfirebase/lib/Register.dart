06e0c772a9fabbcbe056714ac5aedc1c9ff1c6a2
Hassan Ali <hassanali22857@gmail.com>
2024-06-10 08:02:02 +0500

    LoginlogicAdded

--- loginwithfirebase/lib/Register.dart
+++ loginwithfirebase/lib/Register.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:loginwithfirebase/Home.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'package:get/get.dart';
 import 'loginscreen.dart';
@@ -25,122 +28,129 @@ class _RegisterScreenState extends State<RegisterScreen> {
         backgroundColor: Colors.transparent,
         body: Padding(
           padding: const EdgeInsets.only(top: 100),
-          child: Column(
-            children: [
-              (context.screenHeight * 0.1).heightBox,
-              'Register Your Account '
-                  .text
-                  .size(22)
-                  .fontWeight(FontWeight.bold)
-                  .make(),
-              20.heightBox,
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  "Name".text.make(),
-                  TextFormField(
-                    controller: nameController,
-                    decoration: const InputDecoration(
-                        hintText: 'Hassan Ali',
-                        hintStyle: TextStyle(
-                            fontSize: 14, fontWeight: FontWeight.bold),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.zero)),
-                  ),
-                  20.heightBox,
-                  "Email".text.make(),
-                  TextFormField(
-                    controller: emailController,
-                    decoration: const InputDecoration(
-                        hintText: 'Hassan@gmai.com',
-                        hintStyle: TextStyle(
-                            fontSize: 14, fontWeight: FontWeight.bold),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.zero)),
-                  ),
-                  20.heightBox,
-                  "Password".text.make(),
-                  TextFormField(
-                    controller: passwordController,
-                    decoration: const InputDecoration(
-                        hintText: '*******',
-                        hintStyle: TextStyle(
-                            fontSize: 14, fontWeight: FontWeight.bold),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.zero)),
-                  ),
-                  20.heightBox,
-                  Padding(
-                    padding: const EdgeInsets.only(left: 30),
-                    child: SizedBox(
-                      height: 50,
-                      width: 250,
-                      child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.red,
-                              shape: const RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.zero)),
-                          onPressed: () {
-                            var username = nameController.toString().trim();
-                            var useremail = emailController.toString().trim();
-                            var userpassword =
-                                passwordController.toString().trim();
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Column(
+              children: [
+                (context.screenHeight * 0.1).heightBox,
+                'Register Your Account '
+                    .text
+                    .size(22)
+                    .fontWeight(FontWeight.bold)
+                    .make(),
+                20.heightBox,
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    "Name".text.make(),
+                    TextFormField(
+                      controller: nameController,
+                      decoration: const InputDecoration(
+                          hintText: 'Hassan Ali',
+                          hintStyle: TextStyle(
+                              fontSize: 14, fontWeight: FontWeight.bold),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.zero)),
+                    ),
+                    20.heightBox,
+                    "Email".text.make(),
+                    TextFormField(
+                      controller: emailController,
+                      decoration: const InputDecoration(
+                          hintText: 'Hassan@gmail.com',
+                          hintStyle: TextStyle(fontSize: 12, color: Vx.gray400),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.zero)),
+                    ),
+                    20.heightBox,
+                    "Password".text.make(),
+                    TextFormField(
+                      controller: passwordController,
+                      decoration: const InputDecoration(
+                          hintText: '*******',
+                          hintStyle: TextStyle(
+                              fontSize: 14, fontWeight: FontWeight.bold),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.zero)),
+                    ),
+                    20.heightBox,
+                    Padding(
+                      padding: const EdgeInsets.only(left: 30),
+                      child: SizedBox(
+                        height: 50,
+                        width: 250,
+                        child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.red,
+                                shape: const RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.zero)),
+                            onPressed: () {
+                              var username = nameController.text.trim();
+                              var useremail = emailController.text.trim();
+                              var userpassword =
+                                  passwordController.toString().trim();
 
-                            FirebaseAuth.instance
-                                .createUserWithEmailAndPassword(
-                                    email: useremail, password: userpassword)
-                                .then((value) => {
-                                      FirebaseFirestore.instance
-                                          .collection('user')
-                                          .doc()
-                                          .set({
-                                        'userName': username,
-                                        'userEmail': useremail,
-                                        'userPassword': userpassword
-                                      })
-                                    });
-                          },
-                          child: "Register"
-                              .text
-                              .size(22)
-                              .color(Colors.white)
-                              .make()),
+                              FirebaseAuth.instance
+                                  .createUserWithEmailAndPassword(
+                                      email: useremail, password: userpassword)
+                                  .then((value) => {
+                                        print("UserCreated"),
+                                        FirebaseFirestore.instance
+                                            .collection('user')
+                                            .doc()
+                                            .set({
+                                          'userName': username,
+                                          'userEmail': useremail,
+                                        }),
+                                        Get.to(() => HomeScreen()),
+                                        print("DataStore"),
+                                      });
+                            },
+                            child: "Register"
+                                .text
+                                .size(22)
+                                .color(Colors.white)
+                                .make()),
+                      ),
                     ),
-                  ),
-                  30.heightBox,
-                  Padding(
-                    padding: const EdgeInsets.only(left: 35),
-                    child: "Don't Have Account Click Here".text.make(),
-                  ),
-                  20.heightBox,
-                  Padding(
-                    padding: const EdgeInsets.only(left: 30),
-                    child: SizedBox(
-                      height: 50,
-                      width: 250,
-                      child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.red,
-                              shape: const RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.zero)),
-                          onPressed: () {
-                            Get.to(() => const Loginscreen());
-                          },
-                          child:
-                              "Login".text.size(22).color(Colors.white).make()),
+                    30.heightBox,
+                    Padding(
+                      padding: const EdgeInsets.only(left: 35),
+                      child: "Don't Have Account Click Here".text.make(),
                     ),
-                  ),
-                ],
-              )
-            ],
-          )
-              .box
-              .rounded
-              .padding(const EdgeInsets.all(12))
-              .margin(const EdgeInsets.all(8))
-              .color(Colors.white)
-              .width(context.screenWidth - 20)
-              .make(),
+                    20.heightBox,
+                    Padding(
+                      padding: const EdgeInsets.only(left: 30),
+                      child: SizedBox(
+                        height: 50,
+                        width: 250,
+                        child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.red,
+                                shape: const RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.zero)),
+                            onPressed: () {
+                              Get.to(() => const Loginscreen());
+                            },
+                            child: "Login"
+                                .text
+                                .size(22)
+                                .color(Colors.white)
+                                .make()),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            )
+                .box
+                .rounded
+                .padding(const EdgeInsets.all(12))
+                .margin(const EdgeInsets.all(8))
+                .color(Colors.white)
+                .width(context.screenWidth - 20)
+                .make(),
+          ),
         ),
       ),
     );
--- loginwithfirebase/lib/loginscreen.dart
+++ loginwithfirebase/lib/loginscreen.dart
@@ -2,11 +2,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
-import 'package:loginwithfirebase/const/commonbg.dart';
 import 'package:velocity_x/velocity_x.dart';
-
+import 'Home.dart';
 import 'Register.dart';
-import 'const/commontextfield.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Loginscreen extends StatefulWidget {
   const Loginscreen({super.key});
@@ -16,8 +15,8 @@ class Loginscreen extends StatefulWidget {
 }
 
 class _LoginscreenState extends State<Loginscreen> {
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
+  TextEditingController loginemailController = TextEditingController();
+  TextEditingController loginpasswordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -27,73 +26,98 @@ class _LoginscreenState extends State<Loginscreen> {
         backgroundColor: Colors.transparent,
         body: Padding(
           padding: const EdgeInsets.only(top: 160),
-          child: Column(
-            children: [
-              (context.screenHeight * 0.1).heightBox,
-              'Welcome Back'.text.size(22).fontWeight(FontWeight.bold).make(),
-              20.heightBox,
-              Column(
-                children: [
-                  TextFormField(
-                    controller: emailController,
-                    decoration: const InputDecoration(
-                        hintText: 'Hassan@gmai.com',
-                        hintStyle: TextStyle(
-                            fontSize: 14, fontWeight: FontWeight.bold),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.zero)),
-                  ),
-                  20.heightBox,
-                  TextFormField(
-                    controller: passwordController,
-                    decoration: const InputDecoration(
-                        hintText: '*******',
-                        hintStyle: TextStyle(
-                            fontSize: 14, fontWeight: FontWeight.bold),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.zero)),
-                  ),
-                  20.heightBox,
-                  SizedBox(
-                    height: 50,
-                    width: 250,
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.red,
-                            shape: const RoundedRectangleBorder(
-                                borderRadius: BorderRadius.zero)),
-                        onPressed: () {},
-                        child:
-                            "Login".text.size(22).color(Colors.white).make()),
-                  ),
-                  30.heightBox,
-                  "Don't Have Account Click Here".text.make(),
-                  20.heightBox,
-                  SizedBox(
-                    height: 50,
-                    width: 250,
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.red,
-                            shape: const RoundedRectangleBorder(
-                                borderRadius: BorderRadius.zero)),
-                        onPressed: () {
-                          Get.to(() => const RegisterScreen());
-                        },
-                        child:
-                            "Sign up".text.size(22).color(Colors.white).make()),
-                  ),
-                ],
-              )
-            ],
-          )
-              .box
-              .rounded
-              .padding(const EdgeInsets.all(12))
-              .margin(const EdgeInsets.all(8))
-              .color(Colors.white)
-              .width(context.screenWidth - 20)
-              .make(),
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Column(
+              children: [
+                (context.screenHeight * 0.1).heightBox,
+                'Welcome Back'.text.size(22).fontWeight(FontWeight.bold).make(),
+                20.heightBox,
+                Column(
+                  children: [
+                    TextFormField(
+                      controller: loginemailController,
+                      decoration: const InputDecoration(
+                          hintText: 'Hassan@gmai.com',
+                          hintStyle: TextStyle(
+                              fontSize: 14, fontWeight: FontWeight.bold),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.zero)),
+                    ),
+                    20.heightBox,
+                    TextFormField(
+                      controller: loginpasswordController,
+                      decoration: const InputDecoration(
+                          hintText: '*******',
+                          hintStyle: TextStyle(
+                              fontSize: 14, fontWeight: FontWeight.bold),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.zero)),
+                    ),
+                    20.heightBox,
+                    SizedBox(
+                      height: 50,
+                      width: 250,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.red,
+                              shape: const RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.zero)),
+                          onPressed: () async {
+                            var loginEmail = loginemailController.text.trim();
+
+                            var loginPassword =
+                                loginpasswordController.text.trim();
+
+                            try {
+                              final User? firebaseUser = (await FirebaseAuth
+                                      .instance
+                                      .signInWithEmailAndPassword(
+                                          email: loginEmail,
+                                          password: loginPassword))
+                                  .user;
+                              if (firebaseUser != null) {
+                                Get.to(() => const HomeScreen());
+                              }
+                            } on FirebaseAuthException catch (e) {
+                              print('Error $e');
+                            }
+                          },
+                          child:
+                              "Login".text.size(22).color(Colors.white).make()),
+                    ),
+                    30.heightBox,
+                    "Don't Have Account Click Here".text.make(),
+                    20.heightBox,
+                    SizedBox(
+                      height: 50,
+                      width: 250,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.red,
+                              shape: const RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.zero)),
+                          onPressed: () {
+                            Get.to(() => const RegisterScreen());
+                          },
+                          child: "Sign up"
+                              .text
+                              .size(22)
+                              .color(Colors.white)
+                              .make()),
+                    ),
+                  ],
+                )
+              ],
+            )
+                .box
+                .rounded
+                .padding(const EdgeInsets.all(12))
+                .margin(const EdgeInsets.all(8))
+                .color(Colors.white)
+                .width(context.screenWidth - 20)
+                .make(),
+          ),
         ),
       ),
     );
